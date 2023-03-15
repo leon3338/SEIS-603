6e1ba85551eb301b9839e26dbc3a9f83e5728391
leon3338 <58753617+leon3338@users.noreply.github.com>
2023-03-15 13:33:02 -0500

    Delete class_3 2 directory

--- class_3 2/HTT_4_code/_4_7_1_ch03_5.py
+++
@@ -1,8 +0,0 @@
-#
-# HTT Ch 4 code example:
-#
-# Section 4.7: ch03_5
-#
-
-print(list(range(4)))
-print(list(range(1, 5)))
--- class_3 2/HTT_4_code/_4_7_2_ch03_6.py
+++
@@ -1,9 +0,0 @@
-#
-# HTT Ch 4 code example:
-#
-# Section 4.7: ch03_6
-#
-
-print(list(range(0, 19, 2)))
-print(list(range(0, 20, 2)))
-print(list(range(10, -1, -1)))

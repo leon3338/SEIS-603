a1bbd3536cf0014ed129bf6d2045deb61feddddc
leon3338 <58753617+leon3338@users.noreply.github.com>
2023-03-15 13:28:44 -0500

    Add files via upload

---
+++ h_8_starting 2/clone.py
@@ -0,0 +1,13 @@
+#
+#  inplace.py -> H8-1
+#
+
+# try cloning a list w/ [:]
+# compare list with its clone using is
+
+# try cloning a string w/ [:]
+# compare string with its attempted clone using is
+
+# try cloning a tuple w/ [:]
+# compare tuple with its attempted clone using is
+
---
+++ h_8_starting 2/listmod.py
@@ -0,0 +1,13 @@
+#
+#  listmod.py -> H8-2
+#
+
+# read my_list using eval()
+
+# print id(my_list)
+
+# insert 47 as first elt of my_list
+
+# add 47 to end of my_list
+
+# print id(my_list) again

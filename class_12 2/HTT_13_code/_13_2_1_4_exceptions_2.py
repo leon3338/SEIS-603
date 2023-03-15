627df54a7020674a8e1793c7392a433b8fb184c7
leon3338 <58753617+leon3338@users.noreply.github.com>
2023-03-15 13:32:34 -0500

    Delete class_12 2 directory

--- class_12 2/HTT_13_code/_13_2_1_4_exceptions_2.py
+++
@@ -1,8 +0,0 @@
-#
-# HTT Ch 13 code example:
-#
-# Section 13.2.1, example 2: exceptions_2
-#
-
-x = 5
-y = x/0
--- class_12 2/HTT_13_code/_13_2_1_5_exceptions_5.py
+++
@@ -1,5 +0,0 @@
-#
-# HTT Ch 13 code example:
-#
-# Section 13.2.1, example 5: exceptions_5
-#
